38924273a5afd8ff3a0abe36794cc9c9cb53e28b
siyabonga mazibuko <siyabongasamkelociam@gmail.com>
2023-12-23 20:24:19 -0800

    dresses page

--- clientside/myapp/src/dresses/Dresses.js
+++ clientside/myapp/src/dresses/Dresses.js
@@ -1,5 +1,27 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import styled from "styled-components";
+export const Cover = styled.div``;
+export const OnLayBy = styled.div`
+  height: 400px;
+  width: 70%;
+  background-color: red;
+`;
+
 const Dresses = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Cover>
+            <OnLayBy></OnLayBy>
+          </Cover>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Dresses;
